4f7e94ca93ff219917bfd177bea974017aa11984
MaximePremont <maxime.premont@gmail.com>
2020-03-20 17:11:39 +0100

    V1_15_R1 Support
    Plugin now support 1.15.X minecraft version.

--- src/main/java/fr/mpremont/confirmmenu/managers/VersionsManager.java
+++ src/main/java/fr/mpremont/confirmmenu/managers/VersionsManager.java
@@ -9,6 +9,7 @@ import fr.mpremont.confirmmenu.interfaces.versions.V1_12_R1;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_13_R1;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_13_R2;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_14_R1;
+import fr.mpremont.confirmmenu.interfaces.versions.V1_15_R1;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_8_R1;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_8_R2;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_8_R3;
@@ -35,7 +36,9 @@ public class VersionsManager {
 			
 		}
 		
-		if(version.equals("v1_14_R1")) {
+		if(version.equals("v1_15_R1")) {
+			vclass = new V1_15_R1();
+		}else if(version.equals("v1_14_R1")) {
 			vclass = new V1_14_R1();
 		}else if(version.equals("v1_13_R2")) {
 			vclass = new V1_13_R2();
