c19af280837935fb333485dfcce7296b7c8fded1
MaximePremont <maxime.premont@gmail.com>
2020-07-02 16:10:39 +0200

    1.16 Support

--- src/main/java/fr/mpremont/confirmmenu/managers/VersionsManager.java
+++ src/main/java/fr/mpremont/confirmmenu/managers/VersionsManager.java
@@ -10,6 +10,7 @@ import fr.mpremont.confirmmenu.interfaces.versions.V1_13_R1;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_13_R2;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_14_R1;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_15_R1;
+import fr.mpremont.confirmmenu.interfaces.versions.V1_16_R1;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_8_R1;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_8_R2;
 import fr.mpremont.confirmmenu.interfaces.versions.V1_8_R3;
@@ -36,7 +37,9 @@ public class VersionsManager {
 			
 		}
 		
-		if(version.equals("v1_15_R1")) {
+		if(version.equals("v1_16_R1")) {
+			vclass = new V1_16_R1();
+		}else if(version.equals("v1_15_R1")) {
 			vclass = new V1_15_R1();
 		}else if(version.equals("v1_14_R1")) {
 			vclass = new V1_14_R1();
